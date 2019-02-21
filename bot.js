a9583d9fd1cc2441118d0a06904e7ee0c9787466
GabaRx <47712799+GabaRx@users.noreply.github.com>
2019-02-21 07:18:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Retir'ed ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`صدقني بعجبك بس مستحيل إنك تعجبني👋🏻👋🏻`,"http://twitch.tv/Death Shop")
+client.user.setGame(`صدقني بعجبك بس مستحيل إنك تعجبني👋🏻💔`,"http://twitch.tv/Death Shop")
 client.user.setStatus("Online")
 });
 
